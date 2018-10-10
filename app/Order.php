f80e08247c1f0fd4adaecb0f089af6706a3ad116
Fatoni <webind464@gmail.com>
2018-10-10 19:41:46 +0700

    example : many to many

---
+++ app/Order.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+	/**
+	 * table name
+	 * @var string
+	 */
+    protected $table = 'orders';
+
+}
--- database/migrations/2018_10_10_124043_create_table_category.php
+++ database/migrations/2018_10_10_124043_create_table_category.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Student extends Migration
+class CreateTableCategory extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class Student extends Migration
      */
     public function up()
     {
-        // Schema::create("students", function(Blueprint $table) {
-        //     $table->incerements("id");
-        //     $table->string("name", 255)->default("john");
-        //     $table->timestamps();
-        // });
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
     }
 
     /**
